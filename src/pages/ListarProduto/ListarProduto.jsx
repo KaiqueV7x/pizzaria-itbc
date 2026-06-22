cbbf2cdbffeec92b4fef2267a831a4d28f496579
Joao Victor Santos Costa <rm99314@estudante.fieb.edu.br>
2026-06-22 09:03:24 -0300

    Lógica da estilização personalizada

---
+++ src/pages/ListarProduto/ListarProduto.jsx
@@ -0,0 +1,20 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
+
+
+const ListarProduto = () => {
+
+
+    return (
+
+        <div className="container">
+
+            <MenuFuncionario/>
+
+            <p>Lista de Produtos</p>
+
+
+        </div>
+    )
+}
+
+export default ListarProduto
