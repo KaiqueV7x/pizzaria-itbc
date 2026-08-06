2a21b714a022bb19a764c02fbda7d13b48a99bd9
Joao Victor Santos Costa <rm99314@estudante.fieb.edu.br>
2026-08-06 11:35:29 -0300

    Colocando a api Axios

---
+++ src/pages/ListarCategoria/ListarCategoria.jsx
@@ -0,0 +1,17 @@
+import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
+
+
+const ListarCategoria = () =>{
+
+
+    return (
+        <div className="container">
+            <MenuFuncionario/>
+        </div>
+
+    )
+
+
+}
+
+export default ListarCategoria
--- src/pages/ListarProduto/ListarProduto.jsx
+++ src/pages/ListarProduto/ListarProduto.jsx
@@ -3,15 +3,86 @@ import MenuFuncionario from "../MenuFuncionario/MenuFuncionario"
 
 const ListarProduto = () => {
 
+const arrayProdutos = [
+
+{
+    id: 1,
+    nome: "Pizza de Calabresa",
+    precoVenda: 59.90,
+    descricao: "Pizza de calabresa com bastante cebola"
+},
+{
+    id: 2,
+    nome: "Pizza de Mussarela",
+    precoVenda: 69.90,
+    descricao: "Pizza de mussarela com tomates frescos"
+},
+{
+    id: 3,
+    nome: "Pizza de Frango",
+    precoVenda: 63.80,
+    descricao: "Pizza de Frango com catupiry"
+}
+
+
+]
+
+
+
 
     return (
 
         <div className="container">
-
             <MenuFuncionario/>
 
-            <p>Lista de Produtos</p>
+            
 
+            <div className="table-responsive">
+            <table className="table table-bordered table-striped table-hover">
+            <thead className="table-sucess">
+            <tr>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Descrição</th>
+            <th>Ações</th> {/* Nova coluna de Ações */}
+            </tr>
+            </thead>
+            <tbody>
+
+            {arrayProdutos.map((produto)=> (
+            <tr>
+            <td style={{ fontSize: "13px" }}></td>
+            <td style={{ fontSize: "13px" }}>
+                {
+                    new Intl.NumberFormat("pt-BR", {
+
+                    }
+                    )
+                }
+
+            </td>
+            <td style={{ fontSize: "13px" }}></td>
+            <td className="text-center fs-6" style={{ width: "100px" }}>
+            {/* Botão de Editar */}
+            <button
+            className="btn btn-sm btn-primary me-2">
+            <i className="fas fa-pencil-alt"></i>{" "}
+            {/* Ícone de editar */}
+            </button>
+            {/* Botão de Excluir */}
+            <button
+            className="btn btn-sm btn-danger">
+            <i className="fas fa-trash-alt"></i>{" "}
+            {/* Ícone de excluir */}
+            </button>
+            </td>
+            </tr>
+
+            ))}
+
+            </tbody>
+            </table>
+            </div>
 
         </div>
     )
